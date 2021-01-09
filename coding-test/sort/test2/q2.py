d0a867d45449ea30c36c199858eda9e240bcff6d
jslee <dlwhdtjd098@naver.com>
2021-01-09 22:08:06 +0900

    위에서 아래로, 성적이 낮은 순서로 학생 출력하기

--- coding-test/sort/test2/q2.py
+++ coding-test/sort/test2/q2.py
@@ -11,3 +11,23 @@ for i, n in enumerate(arr):
 print(arr)
 
 # 성적이 낮은 순서로 학생 출력하기
+info = {
+    '홍길동': 95,
+    '이순신': 77,
+    '강아지': 100,
+    '가나다': 50,
+}
+index = 0
+result = [k for k in info.keys()]
+
+for i, name in enumerate(result):
+    pivot_name = name
+    pivot_score = info[name]
+    for j, name_ in enumerate(result):
+        score = info[name_]
+        if pivot_score < score:
+            result[i], result[j] = name_, pivot_name
+            pivot_name = name_
+            pivot_score = score
+
+print(result)
